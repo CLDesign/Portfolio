ad20106df7e59b4892046b802507f86a77b337fe
Chen <friedlu@gmail.com>
2016-02-04 23:08:43 -0500

    Updates

--- portfolio.js
+++ portfolio.js
@@ -1,16 +1,41 @@
 $(document).ready(function(){
-  
+    
+    $(".projectThumb").addClass("animationInit");
+    $("#timeLine").addClass("transparent");
+    
+    
+    //part to change slimscroll settings
+    var oldSlimScroll = $.fn.slimScroll;
+    $.fn.slimScroll = function( options ){
+    options.wheelStep = 10;
+    options.touchScrollStep=75;
+    return oldSlimScroll.apply(this, [options] );
+    };
+    
+    //Check window Orientation and pass to paddingTop
+    
+    
+    var TOPPADDING =90;
+   if(Math.abs(window.orientation) === 90) {
+            TOPPADDING = 60;     
+    }
+    
+    
     $('#fullpage').fullpage({
 				sectionsColor: ['none', 'none', 'none', 'none'],//#221728
 				anchors: ['firstPage', 'Work', 'Story', 'Contact'],
 				menu: '#menu',
 				scrollingSpeed: 1000,
                 scrollOverflow: true,
+                paddingTop:TOPPADDING,
+        
+              
                 
         
                 afterRender: function(){
                     var pluginContainer = $(this);
                    //alert("The resulting DOM structure is ready");
+                    
                 },
                  
         
@@ -22,17 +47,34 @@ $(document).ready(function(){
                         $("header").fadeIn( "slow" );
                         
                         // alert("Going to section 1!");
-                        $(".navButtons").css("top",20);
-                        $("#headerLogo, #headerTitle").css("opacity","1");
                         
+                        
+                        $(".navButtons").removeAttr('style');
+                        $(".navButtons").addClass("secondNav");
+                        $("#headerLogo, #headerTitle").css("opacity","1");
+                        $(".projectThumb").addClass("elementsOnLoad");
                         
                     }       
                      
+                    
+                    if(index == 2 && direction =='down'){ 
+                        //going to page 3 
+                        $("#timeLine").addClass("elementsOnLoad");
+                        $(".cd-timeline-block").addClass("timeLineBlockOnLoad delayTransition4");
+                        
+                    }
+                    
                      if(index == 2 && direction =='up'){  //going yp page 1
                        //return false;
                         // this will prevent it going back
                     }
-
+                    
+                    if(index == 4 && direction =='up'){  //going to page 3
+                        
+                         
+                    }
+                    
+                   
                   
                 },
         
@@ -45,10 +87,16 @@ $(document).ready(function(){
                 if(index == 2){
                    //$("#header li:first-child").delay( 800 ).css("color","red");
                      $("#header li:nth-child(1)").addClass("menuSelected").siblings().removeClass("menuSelected");
+                 
                 }
                  if(index == 3){
                    //$("#header li:first-child").delay( 800 ).css("color","red");
                      $("#header li:nth-child(2)").addClass("menuSelected").siblings().removeClass("menuSelected");
+                     
+                     $(".cd-timeline-block").addClass("timeLineBlockOnLoad delayTransition4");
+                     
+                     $("#timeLine").addClass("elementsOnLoad");
+                     
                 }
                  if(index == 4){
                    //$("#header li:first-child").delay( 800 ).css("color","red");
@@ -67,14 +115,14 @@ $(document).ready(function(){
     
     
     $("#header li:first-child").click(function(){
-        //alert( "Handler for .click() called." );
+        
         $.fn.fullpage.moveTo('Work');
         $(this).addClass("menuSelected"); 
         $(this).siblings().removeClass("menuSelected");
     });
     
       $("#header li:nth-child(2)").click(function(){
-        //alert( "Handler for .click() called." );
+        
         $.fn.fullpage.moveTo('Story');
         $(this).addClass("menuSelected");
           $(this).siblings().removeClass("menuSelected");
@@ -88,10 +136,7 @@ $(document).ready(function(){
     });
     
     
-    var titleHeight = $( "#introTitle" ).height(); 
-
-    
-    
+    var titleHeight = $( "#introTitle" ).height();  
     var titileTop = $( "#introTitle" ).offset(); 
     var navInitTop = titileTop.top+titleHeight+10;
     
@@ -101,7 +146,7 @@ $(document).ready(function(){
     
     console.log(navInitTop);
     
-    $(".navButtons").css("top",navInitTop);
+    $(".navButtons").css({transform: 'translateY('+ navInitTop +'px)'});
     
     
         
